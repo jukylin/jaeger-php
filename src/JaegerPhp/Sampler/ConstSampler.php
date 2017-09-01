a83865b183b13a82a0055ca0009c8eeb4f673bff
742161455@qq.com <742161455@qq.com>
2017-09-02 01:38:40 +0800

    添加sampler

---
+++ src/JaegerPhp/Sampler/ConstSampler.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2017/9/2
+ * Time: 上午1:36
+ */
+
+namespace JaegerPhp\Sampler;
+
+
+class ConstSampler implements Sampler
+{
+
+    public function IsSampled()
+    {
+        // TODO: Implement IsSampled() method.
+    }
+
+
+    public function Close()
+    {
+        // TODO: Implement Close() method.
+    }
+
+
+    public function Equal()
+    {
+        // TODO: Implement Equal() method.
+    }
+
+}
---
+++ src/JaegerPhp/Sampler/Sampler.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2017/9/2
+ * Time: 上午1:33
+ */
+
+namespace JaegerPhp\Sampler;
+
+
+interface Sampler
+{
+    public function IsSampled();
+
+    public function Close();
+
+    public function Equal();
+}
