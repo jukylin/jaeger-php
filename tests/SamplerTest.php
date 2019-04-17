c94ba0fdd388f20f564986a162d7c2f50ede8540
Hugo Monteiro <hugo.monteiro@gmail.com>
2019-04-17 18:12:12 +0100

    support php 5.6 with old phpunit

--- tests/SamplerTest.php
+++ tests/SamplerTest.php
@@ -1,19 +1,20 @@
 <?php
+namespace tests;
 
 use PHPUnit\Framework\TestCase;
-
 use Jaeger\Sampler\ConstSampler;
 use Jaeger\Sampler\ProbabilisticSampler;
 
 class SamplerTest extends TestCase
 {
-    public function testConstSampler(){
+    public function testConstSampler()
+    {
         $sample = new ConstSampler(true);
         $this->assertTrue($sample->IsSampled()  == true);
     }
 
-
-    public function testProbabilisticSampler(){
+    public function testProbabilisticSampler()
+    {
         $sample = new ProbabilisticSampler(0.0001);
         $this->assertTrue($sample->IsSampled() !== null);
     }
--- tests/TestSpan.php
+++ tests/TestSpan.php
@@ -1,30 +1,28 @@
 <?php
 
-require_once '../../autoload.php';
+
 
 use OpenTracing\NoopSpanContext;
 use Jaeger\Span;
+use PHPUnit\Framework\TestCase;
 
-class TestSpan extends PHPUnit_Framework_TestCase
+class TestSpan extends TestCase
 {
-
     public function testOverwriteOperationName(){
-        $span = new Span('test1', new NoopSpanContext());
+        $span = new Span('test1', new NoopSpanContext(), []);
         $span->overwriteOperationName('test2');
         $this->assertTrue($span->getOperationName() == 'test2');
     }
 
-
     public function testAddTags(){
-        $span = new Span('test1', new NoopSpanContext());
-        $span->addTags(['test' => 'test']);
+        $span = new Span('test1', new NoopSpanContext(), []);
+        $span->setTags(['test' => 'test']);
         $this->assertTrue((isset($span->tags['test']) && $span->tags['test'] == 'test'));
     }
 
-
     public function testFinish(){
-        $span = new Span('test1', new NoopSpanContext());
-        $span->addTags(['test' => 'test']);
+        $span = new Span('test1', new NoopSpanContext(), []);
+        $span->setTags(['test' => 'test']);
         $span->finish();
         $this->assertTrue(!empty($span->finishTime) && !empty($span->duration));
     }
